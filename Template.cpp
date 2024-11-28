d7f56f6d323b6949baf9282fbe86aeca572ac95b
Jesslyn Caroline <158556535+jesslyn-caroline@users.noreply.github.com>
2024-11-28 14:10:13 +0700

    Update and rename C.cpp to Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+#define vc vector
+#define pb push_back
+
+#define pr pair
+#define fi first
+#define se second
+
+#define pq priority_queue
+
+signed main () {
+    
+}
