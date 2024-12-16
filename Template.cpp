56ba338ff85f8ff133c04caf3e22d7ff8f830da2
Jesslyn Caroline <158556535+jesslyn-caroline@users.noreply.github.com>
2024-12-16 14:35:14 +0700

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 #define vc vector
 #define pb push_back
+#define all(v) sort(v.begin(), v.end())
+#define allr(v) sort(v.rbegin(), v.rend())
 
 #define pii pair <int, int>
 #define fi first
 #define se second
 
 #define pq priority_queue
+
 
 signed main () {
     
