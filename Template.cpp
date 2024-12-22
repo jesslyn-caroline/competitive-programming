dfef3c411eada37f83a431d08f0353170a591e6f
Jesslyn Caroline <158556535+jesslyn-caroline@users.noreply.github.com>
2024-12-22 22:08:23 +0700

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -4,15 +4,23 @@ using namespace std;
 #define int long long 
 
 #define vc vector
+
 #define pb push_back
-#define all(v) sort(v.begin(), v.end())
-#define allr(v) sort(v.rbegin(), v.rend())
+#define pop_front(v) v.erase(v.begin())
+#define erase(v, i) v.erase(v.begin() + i)
+
+#define sort(v) sort(v.begin(), v.end())
+#define sortr(v) sort(v.rbegin(), v.rend())
+#define find(v, i) find(v.begin(), v.end(), i)
 
 #define pii pair <int, int>
 #define fi first
 #define se second
 
 #define pq priority_queue
+
+#define in insert
+#define find(v, i) v.find(i)
 
 
 signed main () {
