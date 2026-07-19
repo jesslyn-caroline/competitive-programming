847c2e446acbeb1aec4f9353870dd52332aca8b4
Jesslyn Caroline <158556535+jesslyn-caroline@users.noreply.github.com>
2026-07-19 22:51:27 +0700

    Update and rename Template.cpp to template.cpp

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio  ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#define int     long long 
+#define double  long double
+
+#define vc      vector
+
+#define pb      push_back
+#define all(v)  v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+
+#define pii     pair <int, int>
+
+#define fi      first
+#define se      second
+
+#define pq      priority_queue
+
+#define imax    LLONG_MAX
+#define imin    LLONG_MIN
+
+#define db(x, y) cout << x << " -> " << y << endl
+
+const int MOD = 1e9 + 7;
+const int MOD2 = 998244353;
+
+
+signed main () {
+    fastio
+    
+    
+}
